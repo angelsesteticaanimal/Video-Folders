8d4fefe282228523507013863fdb22dbeb741fd0
Geison Souza <geisonpixpicpay@gmail.com>
2026-07-02 00:27:41 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-
-    // Document access (pasta de vídeos)
-    implementation("androidx.documentfile:documentfile:1.0.1")
-
-    // Media player ESTÁVEL (não usar versão 1.4.x no GitHub Actions)
-    implementation("androidx.media3:media3-exoplayer:1.2.1")
-    implementation("androidx.media3:media3-ui:1.2.1")
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
